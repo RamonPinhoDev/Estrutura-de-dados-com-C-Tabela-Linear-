d06594f85b7a7f17f8a3df59d503f667d34bf57f
RamonPinhoDev <ramonedeus@outlook.com>
2025-01-27 21:03:55 -0300

    Primeiro commit

---
+++ TabelaLinear/TabelaLinear/Operacoes.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TabelaLinear
+{
+    public class Operacoes
+    {
+        public Operacoes() { }
+        public int[] Add(int[] Array, int valor)
+        {
+            int[] Temporario = new int[Array.Length + 1];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Temporario[i] = Array[i];
+
+            }
+            Temporario[Array.Length] = valor;
+            return Temporario;
+        }
+
+
+        public int[] Insert(int[] Array, int valor, int point)
+        {
+            int[] temp = new int[Array.Length];
+            if (Array.Length > point)
+            {
+                for (int i = 0; i < Array.Length; i++)
+                {
+                    temp[i] = Array[i];
+                }
+
+                temp[point] = valor;
+
+            }
+
+
+            else
+            {
+                Console.WriteLine("O valor informado execede o tamanho do Array");
+
+            }
+
+            return temp;
+        }
+
+
+        public int[] Remover(int[] Array, int point)
+        {
+
+            int[] temp = new int[Array.Length - 1];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (i < point)
+
+
+                    temp[i] = Array[i];
+
+
+                if (i > point)
+                    temp[i - 1] = Array[i];
+                }
+            return temp;
+        }
+
+    }
+}
+
+
+
---
+++ TabelaLinear/TabelaLinear/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System;
+
+
+
+namespace TabelaLinear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 90, 70, 50, 80, 60, 86 };
+
+            //Anexar tabela linnear
+            var op = new Operacoes();
+            array = op.Add(array, 75);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ",");
+            }
+
+            Console.WriteLine();
+
+            //Inserção de item em tabela linear
+            int[] array2 = { 90, 70, 50, 80, 60, 86 };
+            array2 = op.Insert(array2, 75, 2);
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write(array2[i] + ",");
+            }
+
+            Console.WriteLine();
+
+            int[] array3 = { 90, 70, 50, 80, 60, 86 };
+
+            array3 = op.Remover(array3, 2);
+
+            for(int i = 0;i < array3.Length; i++)
+            {
+                Console.Write(array3[i] + ",");
+            }
+
+
+
+
+
+
+        }
+        
+        
+    }
+}
+
+
